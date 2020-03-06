ef2844c611245cc061f5f48b500492c3b5c1013a
ait ihya <hajaraitihya@gmail.com>
2020-03-07 00:50:40 +0100

    app

---
+++ exrcice4_1/src/test/java/fr/uvsq/exrcice4_1/AppTest.java
@@ -0,0 +1,33 @@
+package fr.uvsq.exrcice4_1;
+
+import java.time.LocalDate;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+	@Before
+	
+    @Test
+    public void testApp()
+    {
+		Personnel p1;
+		Personnel p2;
+		Personnel p3;
+		Personnel p4;
+		CompositePersonnels cp1;
+		p1= new Personnel.Builder(0, "hajar", "hajar",null).build();
+		p2= new Personnel.Builder(0, "hajar", "hajar",null).build();
+		p3= new Personnel.Builder(0, "hajar", "hajar",null).build();
+		p4= new Personnel.Builder(0, "hajar", "hajar",null).build();
+				
+		
+		
+    	
+    }
+}
